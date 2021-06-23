13ff9b84d6c63018be2b45a819c9910d8e728f51
hyoukhoon <hyoukhoon@gmail.com>
2021-06-23 14:55:04 +0900

    fdsafd

---
+++ admin_area/del.php
@@ -0,0 +1,29 @@
+<?php session_start();
+include $_SERVER['DOCUMENT_ROOT']."/inc/dbcon.php";
+
+
+$num=$_GET['num'];
+
+	$result=$mysqli->query("select * from news where num='$num' $where") or die("3:".$mysqli->error);
+	$rs = $result->fetch_object();
+	$file=$rs->file_list;
+	if($file){
+		$delFile=explode(",",$file);
+		for($i=0;$i<sizeof($delFile);$i++){
+			unlink($_SERVER['DOCUMENT_ROOT'].$delFile[$i]);
+		}
+	}
+	
+	$query="delete from news where num='$num' $where";
+	$sql1=$mysqli->query($query) or die("3:".$mysqli->error);
+
+
+	if($sql1){
+
+		location_is('list.php','','삭제했습니다');
+	}else{
+		location_is('','','다시 시도해주십시오.');
+	}
+
+	
+?>
