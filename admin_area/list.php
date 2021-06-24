090d3603b15d98346cf076afd1bffa2d743ec001
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-24 10:41:52 +0900

    fasfd

--- admin_area/list.php
+++ admin_area/list.php
@@ -71,72 +71,7 @@ while($rs = $result->fetch_object()){
   </tbody>
 </table>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-        </div>
-      </th>
-      <th scope="col">Lorem</th>
-      <th scope="col">Ipsum</th>
-      <th scope="col">Dolor</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-        </div>
-      </th>
-      <td>Sit</td>
-      <td>Amet</td>
-      <td>Consectetur</td>
-    </tr>
-    <tr>
-      <th scope="row">
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-        </div>
-      </th>
-      <td>Adipisicing</td>
-      <td>Elit</td>
-      <td>Sint</td>
-    </tr>
-    <tr>
-      <th scope="row">
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-        </div>
-      </th>
-      <td>Hic</td>
-      <td>Fugiat</td>
-      <td>Temporibus</td>
-    </tr>
-  </tbody>
-</table>
+
 <!-- 페이징 -->
                 <div>
                     <div class="pagination-area">
