c5f97500e6e7c403d7f85b5e868f43fc29407999
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-24 17:21:06 +0900

    FDS

--- adminPage/write.php
+++ adminPage/write.php
@@ -51,10 +51,13 @@ if($num){
                                 <div id="summernote"><?echo content_is2($rs->content);?></div>
                                 </div>
                                 <div class="form-group">
-                                    <div id="attach_site">
-                                      썸네일 : 
-                                        <div id="attachFiles">
-                                        </div>
+                                <div class="btn btn-default btn-file">
+                                  <i class="fas fa-paperclip"></i> Attachment
+                                  <input type="file" name="attachment">
+                                </div>
+                                    <div id="attach_site" class="btn btn-default btn-file">
+                                    <div id="attachFiles"></div>
+                                      <i class="fas fa-paperclip"></i> 썸네일 : 
                                         <input type="file" multiple class="form-input" name="afile" id="afile" />
                                     </div>
                                 </div>
