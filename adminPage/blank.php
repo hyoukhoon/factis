70145cc63a511f7b996de0d2f4193eac5d3d19c0
hyoukhoon <hyoukhoon@gmail.com>
2021-06-25 10:17:56 +0900

    fdsaf

--- adminPage/blank.php
+++ adminPage/blank.php
@@ -1,23 +1,18 @@
 <?php include $_SERVER["DOCUMENT_ROOT"]."/adminPage/header.php";
 ?>
-
             <!-- Main Content -->
             <div id="content">
-
                 <!-- Topbar -->
                 <?php include $_SERVER["DOCUMENT_ROOT"]."/adminPage/topbar.php";?>
                 <!-- End of Topbar -->
-
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-
                 <!-- 시작 -->
 
                     
                 <!-- 끝 -->
                 </div>
                 <!-- /.container-fluid -->
-
             </div>
             <!-- End of Main Content -->
 
