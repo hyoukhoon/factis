abd3f8599b68d9158c47802ea73e0a050d8cb62c
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-28 15:47:08 +0900

    dsafd

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
+<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/dbcon.php";
 $result = $mysqli->query("select * from news where ismain=1 order by num desc limit 30");
 while($rs = $result->fetch_object()){
 	$rsc[$rs->place]=$rs;
