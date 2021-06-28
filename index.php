779009b50c02f65f55a16f5500d7a5a37d1e34cb
hyoukhoon <hyoukhoon@gmail.com>
2021-06-28 19:24:35 +0900

    dfa

--- index.php
+++ index.php
@@ -491,74 +491,7 @@ while($rs = $result->fetch_object()){
 	
 
 
-	<!--::::: MIX CAROUSEL AREA START :::::::-->
-	<div class="half_bg1 mix_area">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="mix_carousel">
-						<!--CAROUSEL START-->
-						<div class="single_mix_carousel owl-carousel nav_style3">
-							<div class="single_post post_type6 post_type9">
-								<div class="post_img gradient1">
-									<div class="img_wrap">
-										<a class="play_btn" href="#">
-											<img src="assets/img/bg/black_white1.jpg" alt="">
-										</a>
-									</div>	<span class="tranding">
-										<i class="fas fa-play"></i>
-									</span>
-								</div>
-								<div class="single_post_text">
-									<div class="meta">	<a href="#">TECHNOLOGY</a>
-										<a href="#">March 26, 2020</a>
-									</div>
-									<h4><a href="video_post1.html">Success is not a good food failure makes you humble</a></h4>
-								</div>
-							</div>
-							<div class="single_post post_type6 post_type9">
-								<div class="post_img gradient1">
-									<div class="img_wrap">
-										<a href="#" class="play_btn">
-											<img src="assets/img/bg/black_white2.jpg" alt="">
-										</a>
-									</div>	<span class="tranding left">
-										<i class="fas fa-bolt"></i>
-									</span>
-								</div>
-								<div class="single_post_text">
-									<div class="meta">	<a href="#">TECHNOLOGY</a>
-										<a href="#">March 26, 2020</a>
-									</div>
-									<h4><a href="post1.html">Success is not a good food failure makes you humble</a></h4>
-								</div>
-							</div>
-							<div class="single_post post_type6 post_type9">
-								<div class="post_img gradient1">
-									<div class="img_wrap">
-										<a href="#" class="play_btn">
-											<img src="assets/img/bg/black_white2.jpg" alt="">
-										</a>
-									</div>	<span class="tranding left">
-										<i class="fas fa-bolt"></i>
-									</span>
-								</div>
-								<div class="single_post_text">
-									<div class="meta">	<a href="#">TECHNOLOGY</a>
-										<a href="#">March 26, 2020</a>
-									</div>
-									<h4><a href="post1.html">Success is not a good food failure makes you humble</a></h4>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!--CAROUSEL END-->
-				</div>
-			</div>
-		</div>
-		<div class="space-60"></div>
-	</div>
-	<!--::::: MIX CAROUSEL AREA END :::::::-->
+	
 
 	<!--::::: VIDEO POST AREA START :::::::-->
 	<div class="video_posts pt30 half_bg60">
