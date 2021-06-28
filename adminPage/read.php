b74d51cf56f5678f067a364b00d35c1057beed93
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-28 10:24:51 +0900

    fdas

--- adminPage/read.php
+++ adminPage/read.php
@@ -65,47 +65,22 @@ $rs = $result->fetch_object();
                         <span class="float-right text-muted">127 likes - 3 comments</span>
                     </div>
                     <!-- /.card-body -->
-                    <div class="card-footer card-comments">
-                        <div class="card-comment">
-                        <!-- User image -->
-                        <img class="img-circle img-sm" src="../dist/img/user3-128x128.jpg" alt="User Image">
-
-                        <div class="comment-text">
-                            <span class="username">
-                            Maria Gonzales
-                            <span class="text-muted float-right">8:03 PM Today</span>
-                            </span><!-- /.username -->
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page when looking at its layout.
-                        </div>
-                        <!-- /.comment-text -->
-                        </div>
-                        <!-- /.card-comment -->
-                        <div class="card-comment">
-                        <!-- User image -->
-                        <img class="img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="User Image">
-
-                        <div class="comment-text">
-                            <span class="username">
-                            Luna Stark
-                            <span class="text-muted float-right">8:03 PM Today</span>
-                            </span><!-- /.username -->
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page when looking at its layout.
-                        </div>
-                        <!-- /.comment-text -->
-                        </div>
-                        <!-- /.card-comment -->
-                    </div>
+                    
                     <!-- /.card-footer -->
                     <div class="card-footer">
-                        <form action="#" method="post">
-                        <img class="img-fluid img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="Alt Text">
-                        <!-- .img-push is used to add margin to elements next to floating images -->
-                        <div class="img-push">
-                            <input type="text" class="form-control form-control-sm" placeholder="Press enter to post comment">
+                        <div style="float:right;">
+                            <a href="write.php">
+                                <a href="list.php">
+                                    <button type="button" class="btn btn-secondary">목록</button>
+                                </a>
+                                <a href="write.php?num=<?php echo $rs->num;?>">
+                                    <button type="button" class="btn btn-secondary">수정</button>
+                                </a>
+                                <a href="del.php?num=<?php echo $rs->num;?>" onclick="return confirm('삭제하시겠습니까?');">
+                                    <button type="button" class="btn btn-secondary">삭제</button>
+                                </a>
+                            </a>
                         </div>
-                        </form>
                     </div>
                     <!-- /.card-footer -->
                     </div>
