952b0682f87538898f30e0f98bd02d3563b7bb90
hyoukhoon <hyoukhoon@gmail.com>
2021-06-28 19:33:50 +0900

    afds

--- adminPage/write.php
+++ adminPage/write.php
@@ -48,7 +48,11 @@ if($num){
                         <label style="font-weight:700;">간략글</label>
                         <textarea class="form-control" rows="3" id="main_text" placeholder="메인에 나타나는 간략 설명"><?echo stripslashes($rs->main_text);?></textarea>
                     </div>
-
+                    <div class="form-group">
+                      <input class="custom-control-input custom-control-input-danger" type="radio" id="place" name="place" value="trending_news">Trending News / 
+                      <input class="custom-control-input custom-control-input-danger" type="radio" id="place" name="place" value="second_news">Second News / 
+                      <input class="custom-control-input custom-control-input-danger" type="radio" id="place" name="place" value="feature_news">Feature News
+                    </div>
                     <div class="form-group">
                         <div id="summernote"><?echo content_is2($rs->content);?></div>
                     </div>
