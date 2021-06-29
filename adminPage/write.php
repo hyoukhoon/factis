a674f01e5b1ac48d459971f31ce22d1c5625ec0b
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-29 15:55:06 +0900

    fdsaf

--- adminPage/write.php
+++ adminPage/write.php
@@ -17,6 +17,7 @@ if($num){
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 
+<link href="/adminPage/css/adminlte.css" rel="stylesheet">
             <!-- Main Content -->
             <div id="content">
                 <!-- Topbar -->
