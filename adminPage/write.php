b6a6c9feed7bdba0a9c64e8082aec252edde31d8
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-29 15:24:50 +0900

    fdsad

--- adminPage/write.php
+++ adminPage/write.php
@@ -73,6 +73,25 @@ if($num){
                     
                     <div class="form-group">
                         <label for="exampleInputFile">썸네일</label>
+                        <div class="card card-danger">
+                          <div class="card-header">
+                            <h3 class="card-title">Different Width</h3>
+                          </div>
+                          <div class="card-body">
+                            <div class="row">
+                              <div class="col-3">
+                                <input type="text" class="form-control" placeholder=".col-3">
+                              </div>
+                              <div class="col-4">
+                                <input type="text" class="form-control" placeholder=".col-4">
+                              </div>
+                              <div class="col-5">
+                                <input type="text" class="form-control" placeholder=".col-5">
+                              </div>
+                            </div>
+                          </div>
+                          <!-- /.card-body -->
+                        </div>
                         <div id="thumbnails">
                           <?php
                             $fn=explode(",",$rs->fn1);
