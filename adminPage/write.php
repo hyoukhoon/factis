6c587fbb7598335dbe6f4c9f9fbdd06a13f1d070
hyoukhoon <hyoukhoon@gmail.com>
2021-06-30 10:55:21 +0900

    fdsaf

--- adminPage/write.php
+++ adminPage/write.php
@@ -75,7 +75,7 @@ if($num){
                     <div class="card-footer bg-white">
                       <ul class="mailbox-attachments d-flex align-items-stretch clearfix">
 
-                        <li>
+                        <!-- <li>
                           <span class="mailbox-attachment-icon"><i class="far fa-file-pdf"></i></span>
 
                           <div class="mailbox-attachment-info">
@@ -96,8 +96,18 @@ if($num){
                                   <a href="#" class="btn btn-default btn-sm float-right"><i class="fas fa-cloud-download-alt"></i></a>
                                 </span>
                           </div>
-                        </li>
+                        </li> -->
 
+                        <li>
+                          <span class="mailbox-attachment-icon has-img"><img src="http://news.evvo.kr/news/upImages/cs_202106301047581515.png" style="height:132px;padding:10px; width:198px;" alt="Attachment"></span>
+                          <div class="mailbox-attachment-info">
+                            <a href="#" class="mailbox-attachment-name"><i class="fas fa-camera"></i>첨부파일</a>
+                                <span class="mailbox-attachment-size clearfix mt-1">
+                                  <span>2.67 MB</span>
+                                  <a href="#" class="btn btn-default btn-sm float-right"><i class="fas fa-cloud-download-alt"></i></a>
+                                </span>
+                          </div>
+                        </li>
                         <li>
                           <span class="mailbox-attachment-icon has-img"><img src="http://news.evvo.kr/news/upImages/cs_202106301047581515.png" style="height:132px;padding:10px; width:198px;" alt="Attachment"></span>
                           <div class="mailbox-attachment-info">
