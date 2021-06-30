b718417edae9b1722c52fd7585372f15e21d0844
hyoukhoon <hyoukhoon@gmail.com>
2021-06-30 10:57:10 +0900

    fdasf

--- adminPage/write.php
+++ adminPage/write.php
@@ -113,7 +113,9 @@ if($num){
                           <div class="mailbox-attachment-info">
                             <a href="#" class="mailbox-attachment-name"><i class="fas fa-camera"></i>첨부파일</a>
                                 <span class="mailbox-attachment-size clearfix mt-1">
-                                  <span>2.67 MB</span>
+                                  <button type="button" class="btn btn-default btn-sm">
+                                    <i class="far fa-trash-alt"></i>
+                                  </button>
                                   <a href="#" class="btn btn-default btn-sm float-right"><i class="fas fa-cloud-download-alt"></i></a>
                                 </span>
                           </div>
