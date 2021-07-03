dec6e93d52b1c8107cbf5bef543312adc4d8c6ac
hyoukhoon <hyoukhoon@gmail.com>
2021-07-03 10:09:08 +0900

    fdafd

--- adminPage/write.php
+++ adminPage/write.php
@@ -382,15 +382,11 @@ function attachFile(file) {
             var img="<li id='"+$.trim(data.fid)+"'><span class='mailbox-attachment-icon has-img'><img src='"+$.trim(data.fn)+"' style='height:132px;padding:10px; ' alt='Attachment'></span><div class='mailbox-attachment-info'><a href='#' class='mailbox-attachment-name'><i class='fas fa-camera'></i>첨부파일</a><span class='mailbox-attachment-size clearfix mt-1'><a href='javascript:;' onclick='delAttch(\""+$.trim(data.fid)+"\",\""+$.trim(data.fn)+"\",\""+$.trim(data.ofn)+"\")' class='float-right'><button type='button' class='btn btn-default btn-sm'><i class='far fa-trash-alt'></i></button></a></span></div></li>";
             $("#thumbnails").append(img);
             var attachFile=$("#attachFiles").val();
-            if(attachFile){
-              attachFile=attachFile+",";
-            }
+            attachFile=attachFile+",";
             $("#attachFiles").val(attachFile+$.trim(data));
 
             var attachName=$("#attachNames").val();
-            if(attachName){
-              attachName=attachName+",";
-            }
+            attachName=attachName+",";
             $("#attachNames").val(attachName+$.trim(data.ofn));
 
           }
@@ -417,13 +413,16 @@ function delAttch(fid,fn,ofn){
 
         if(data.result==1){
           var attachFile=$("#attachFiles").val();
-          attachFile=attachFile.replace(fn+",","");
+          if(attachFile){
+            attachFile=attachFile.replace(fn+",","");
+          }
           $("#attachFiles").val(attachFile);
 
           var attachName=$("#attachNames").val();
-          attachName=attachName.replace(ofn+",","");
+          if(attachName){
+            attachName=attachName.replace(ofn+",","");
+          }
           $("#attachNames").val(attachName);
-
           $("#"+fid).hide();
         }else{
           alert('다시 시도해 주십시오.');
