616721a868e6096a8a703c97586199a40dcee969
hyoukhoon <hyoukhoon@cnspartner.com>
2021-07-06 14:15:16 +0900

    dsfds

--- post.php
+++ post.php
@@ -85,104 +85,13 @@ if(!$rs->num){
 						</div>
 					</div>
 					<div class="space-20"></div>
-					<p>Entilators will be taken from certain New York hospitals and redistributed to the worst-hit parts of the state under an order to be signed by Governor Andrew Cuomo.
-						<br>
-						<br>New York saw its highest single-day increase in deaths, up by 562 to 2,935 - nearly half of all virus-related US deaths recorded yesterday. The White House may advise those in virus hotspots to wear face coverings in public to help stem the spread.\
-						<br>
-						<br>The US now has 245,658 Covid-19 cases.
-						<br>
-						<br>A shortage of several hundred ventilators in New York City, the epicentre of the outbreak in the US, prompted Mr Cuomo to say that he will order the machines be taken from various parts of the state and give them to harder-hit areas.
-						<br>
-						<br>Amid a deepening crisis, top health official <span class="bold"> Dr Anthony Fauci</span> has said he believes all states should issue stay-at-home orders.
-						<br>
-						<br>“I don’t understand why that’s not happening,” Dr Fauci told CNN on Thursday. “If you look at what’s going on in this country, I just don’t understand why we’re not do ingthat.”
-						<br>
-						<br>“You’ve got to put your foot on the accelerator to bring that number down,” he added, referring to infection and death rates.</p>
+
+					<p>
+							<?php echo stripslashes($rs->content);?>
+					</p>
+					
 					<div class="space-40"></div>
-					<div class="points">
-						<ul>
-							<li>Should more of us wear face masks?</li>
-							<li>Why some countries wear face masks and others don’t</li>
-							<li>Coronavirus: Are homemade face masks safe?</li>
-						</ul>
-					</div>
-					<div class="space-40"></div>
-					<p>The comments from Dr Fauci, who heads the National Institute of Allergy and Infectious Diseases, appeared to contradict those of President Trump, who has consistently dismissed the notion of a nationwide lockdown.
-						<br>
-						<br>“It’s awfully tough to say, ‘close it down.’ We have to have a little bit of flexibility,” Mr Trump said on Wednesday.</p>
-					<div class="space-40"></div>
-					<h3>What’s the debate over masks?</h3>
-					<div class="space-20"></div>
-					<p>Both the US Centers for Disease Control (CDC) and the World Health Organization (WHO) are reassessing their guidance on face masks, as experts race to find ways to fight the highly contagious virus.
-						<br>
-						<br>Covid-19 is carried in airborne droplets from people coughing or sneezing, but there is some dispute over how far people should distance themselves from each other, and whether masks are useful when used by the public.</p>
-					<div class="space-40"></div>
-					<div class="row">
-						<div class="col-md-6">
-							<img src="assets/img/blog/smail1.jpg" alt="">
-						</div>
-						<div class="col-md-6">
-							<p>The WHO advises that ordinary face masks are only effective if combined with careful hand-washing and social-distancing, and so far it does not recommend them generally for healthy people.
-								<br>
-								<br>However, More and more health experts now say there are benefits. They argue that the public use of masks can primarily help by preventing asymptomatic patients - people who have been infected with Covid-19 but are not aware, and not displaying any symptoms - from unknowingly spreading the virus to others.</p>
-						</div>
-					</div>
-					<div class="space-40"></div>
-					<p>Masks may also help lower the risk of individuals catching the virus through the droplets from another person’s sneeze or a cough - and people can be taught how put masks on and take them off correctly, they argue.
-						<br>
-						<br>On Thursday New York mayor Bill de Blasio urged all New Yorkers to cover their faces when outside and near others, but not to use surgical masks, which are in short supply.
-						<br>
-						<br>“It could be a scarf. It could be something you create yourself at home. It could be a bandana,” he said. Governor Cuomo weighed in on Friday, saying “i think it’s fair to say that the masks couldn’t hurt unless they gave you a false sense of security.”
-						<br>
-						<br>Meanwhile, residents in Laredo, Texas will now face a $1,000 (£816) fine if they fail to cover their noses and mouths while outside, after city officials issued an emergency ordinance to its approximately 250,000 residents this week.</p>
-					<div class="space-40"></div>
-					<h3>Which states are not in lockdown?</h3>
-					<div class="space-20"></div>
-					<p>Both the US Centers for Disease Control (CDC) and the World Health Organization (WHO) are reassessing their guidance on face masks, as experts race to find ways to fight the highly contagious virus.
-						<br>
-						<br>Covid-19 is carried in airborne droplets from people coughing or sneezing, but there is some dispute over how far people should distance themselves from each other, and whether masks are useful when used by the public.</p>
-					<div class="space-40"></div>
-					<img src="assets/img/blog/big1.jpg" alt="">	<p class="img_desc"><span>I just had a baby - now I’m going to the frontline.</span></p>
-					<div class="space-40"></div>
-					<p>Masks may also help lower the risk of individuals catching the virus through the droplets from another person’s sneeze or a cough - and people can be taught how put masks on and take them off correctly, they argue.
-						<br>
-						<br>On Thursday New York mayor Bill de Blasio urged all New Yorkers to cover their faces when outside and near others, but not to use surgical masks, which are in short supply.
-						<br>
-						<br>Meanwhile, residents in Laredo, Texas will now face a $1,000 (£816) fine if they fail to cover their noses and mouths while outside, after city officials issued an emergency ordinance to its approximately 250,000 residents this week.</p>
-					<div class="space-40"></div>
-					<div class="row">
-						<div class="col-md-5">
-							<img src="assets/img/blog/quote_1.jpg" alt="">
-						</div>
-						<div class="col-md-7">
-							<div class="qhote">
-								<img src="assets/img/icon/q.png" alt="">
-								<p>I must explain to you how all this mistake idea denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. .</p>
-								<div class="author">
-									<div class="author_img">
-										<div class="author_img_wrap">
-											<img src="assets/img/author/author2.png" alt="">
-										</div>
-									</div>	<a href="#">Shuvas Chandra</a>
-									<ul>
-										<li>Founder at Seative Digital</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="space-40"></div>
-					<p>The next day I came back to my team and said, This is what I just heard, we have to get ready, he said. We knew that it wasn’t going to be long before we were going to have to deal with it.
-						<br>
-						<br>Mr. Hogan has also leaned on his wife, Yumi Hogan, a Korean immigrant, who was also at the governor’s convention, which included a dinner at the Korean ambassador’s home. As the first Korean first lady in American history, Ms. Hogan has become something of an icon in South Korea. I just grabbed my wife and said, Look, you speak Korean. You know the president. You know the first lady. You know the ambassador. Let’s talk to them in Korean, and tell them we need their help. Companies in South Korea said would tests.</p>
-					<div class="space-40"></div>
-					<img src="assets/img/blog/big2.jpg" alt="">
-					<div class="space-40"></div>
-					<p>In global terms the US has the most Covid-19 cases - more than 245,000. And on Thursday the US authorities said more than 1,000 had died in the past 24 hours - the highest daily toll so far in the world.
-						<br>
-						<br>Hospitals and morgues in New York are struggling to cope with the pandemic, and New York Governor Andrew Cuomo has warned that New York risks running out of ventilators for patients in six days.</p>
-					<div class="space-40"></div>
-					<div class="tags">
+					<!-- <div class="tags">
 						<ul class="inline">
 							<li class="tag_list"><i class="fas fa-tag"></i> tags</li>
 							<li><a href="#">Health</a>
@@ -192,7 +101,7 @@ if(!$rs->num){
 							<li><a href="#">Corona</a>
 							</li>
 						</ul>
-					</div>
+					</div> -->
 					<div class="space-40"></div>
 					<div class="border_black"></div>
 					<div class="space-40"></div>
